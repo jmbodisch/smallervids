e8b1dcc913c5b23fc0d50d386e4f46d285aed532
jmbodisch <boingloing@gmail.com>
2021-02-05 10:34:12 -0500

    basic testing functionality

--- smallervids.js
+++ smallervids.js
@@ -1,1 +1,14 @@
-document.body.style.border = "5px solid red";
+setTimeout(function() {
+    Array.from(document.getElementsByTagName("ytd-compact-video-renderer")).forEach(element => {
+        randomBgColor(element);
+    });
+
+    Array.from(document.getElementsByTagName("ytd-rich-item-renderer")).forEach(element => {
+        randomBgColor(element);
+    });
+}, 3000);
+
+function randomBgColor(element) {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    element.style.backgroundColor = "#" + randomColor;
+}
