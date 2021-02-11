1c4a9bedce7a6e0ddb4260b615d87135b51eaece
jmbodisch <boingloing@gmail.com>
2021-02-11 14:40:47 -0500

    Recognize the views counter in blocks

--- smallervids.js
+++ smallervids.js
@@ -1,11 +1,18 @@
 setTimeout(function() {
-    Array.from(document.getElementsByTagName("ytd-compact-video-renderer")).forEach(element => {
-        randomBgColor(element);
+    var metaBlocks = document.querySelectorAll("span.ytd-video-meta-block");
+    var metaArray = Array.from(metaBlocks);
+
+    console.log(metaArray.length);
+
+    metaArray.forEach(function(block){
+        if (block.innerHTML.search("views") != -1) {
+            var compact = block.closest("ytd-compact-video-renderer");
+            let item = block.closest("ytd-rich-item-renderer");
+            if (compact != null) randomBgColor(compact);
+            if (item != null) randomBgColor(item);
+        };
     });
 
-    Array.from(document.getElementsByTagName("ytd-rich-item-renderer")).forEach(element => {
-        randomBgColor(element);
-    });
 }, 3000);
 
 function randomBgColor(element) {
